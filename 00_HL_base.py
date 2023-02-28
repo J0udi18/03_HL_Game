709dcfd8c48104ccfafbc364ac8dcccc45e2cfff
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-01 10:04:54 +1300

    Add files via upload

--- 00_HL_base.py
+++ 00_HL_base.py
@@ -8,5 +8,57 @@
 # lower and upper bound)
 
 
- # Number checking function goes here
+# Number checking function goes here
+def choice_checker(question, low=None, high=None, respoonse=None):
+
+    situation = ""
+    if low is not None and high is not None:
+        situation = "both"
+    elif low is not None and high is not None:
+        situation = "low only"
+
+    while True:
+
+        try:
+            response = int(input(question))
+
+            # checks input is not too high or
+            # too low if a both upper and lower bounds
+            # are specified
+            if situation == "both":
+                if response <low or respoonse > high:
+                    print("please enter a number between"
+                          "{} and {}".format(low, high))
+                    continue
+
+            # checks input is not too low
+            elif situation == "low only":
+                if response < low:
+                    print("please enter a number that is more "
+                          "than (or equal to) {}".format(low))
+                    continue
+
+            return response
+
+        # checks input is a integer
+        except ValueError:
+            print("please enter an integer")
+            continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

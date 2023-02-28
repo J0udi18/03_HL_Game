5e827b6bceccd212c10bc4384aad13cfad2ce375
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-01 09:39:11 +1300

    Add files via upload

---
+++ 00_HL_base.py
@@ -0,0 +1,12 @@
+# Hl component 1 _ Get (and Check) user input
+
+# To Do
+# Check a lowest is an integer (any integer)
+# Check that highest is more than lowest (lower bound only)
+# Check that rounds is more than 1 (upper bound only)
+# Check that guess is between lowest and highest (
+# lower and upper bound)
+
+
+ # Number checking function goes here
+

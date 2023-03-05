6b05250944f2ba15f5d28f18800dec6a87a185fc
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-06 12:30:48 +1300

    Add files via upload

--- 12_Higher_Lower_v3.py
+++ 12_Higher_Lower_v3.py
@@ -1,2 +1,23 @@
-# Main Routine goes here...
-("welcome to the Higher Lower Game", "*")
+# random import
+
+# play instructions
+def instructions():
+    print("for each game you will be asked to...")
+    print()
+    print("- Enter a 'low' and 'high' number."
+          "The computer will randomly generate a"
+          "'secret' number between you two chosen numbers."
+          "It will use these numbers for all"
+          "the rounds in a given game.")
+    print()
+    print("- The computer will calculate how many guesses you are allowed"
+          "- enter the number of rounds you want to play"
+          "- guess the secret number")
+
+    print("Good Luck!")
+
+
+play = input("press <Enter> to begin...").lower()
+
+# If they press <Enter>, output 'program continues'
+print("please answer yes / no")
